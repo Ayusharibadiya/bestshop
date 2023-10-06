a1e3eb2330f6329275327a98b8d475ca49891e16
Ayusharibadiya <ayusharibadiya1711@gmail.com>
2023-10-06 08:34:22 +0530

    shop

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('#slider1').owlCarousel({
+    items:1,
+    dots:true,
+    navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+    nav:true
+})
+
+
+$('#slider2').owlCarousel({
+    items:3,
+    margin:30,
+    dotsEach: true
+})
+
+$('#slider3').owlCarousel({
+    items:1,
+    navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+    nav:true
+})
